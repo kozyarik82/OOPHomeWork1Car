91334fd5f4494c45526c239052a47a1c11bd898b
kozyar <kozyarik@gmail.com>
2022-10-26 14:28:10 +0200

    задачи решены

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
+import transport.Car;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        Car lada = new Car("Lada", "Granta", "yellow", 2015, "Russia",1.7);
-        Car audi = new Car("Audi", "A8 50L TDI quattro", "black", 2020, "Germany", 3);
-        Car bmw = new Car("BMW", "Z8", "black", 2021,"Germany",3);
-        Car kia = new Car("Kia", "Sportage 4",  "red",2018, "South Korea", 2.4);
-        Car hyundai = new Car("Hyundai", "Avante", "orange", 0, "South Korea", 1.6);
+        Human maksim = new Human(1987," Максим ","Минск","бренд-менеджер");
+        Human anna = new Human(1993,"Аня","Москва",  "методист образовательных программ");
+        Human kate = new Human(1994,"Катя", "Калининград",  "продакт-менеджер");
+        Human artyom = new Human(1995,"Артем", "Москва",  "директор по развитию бизнеса");
+        Human vladimir = new Human(2001," Владимир "," Казань ", null);
 
-        lada.vehicleInformation();
-        audi.vehicleInformation();
-        bmw.vehicleInformation();
-        kia.vehicleInformation();
-        hyundai.vehicleInformation();
+
+        maksim.greetings();
+        anna.greetings();
+        kate.greetings();
+        artyom.greetings();
+        vladimir.greetings();
+
+        Car lada = new Car("Lada", "Granta", "yellow", 2015, "Russia",1.7f,"automatic","sedan","М215ВК263",5,true,new Car.Key(true,true), new Car.Insurance(LocalDate.now().plusMonths(4),7000.0f,"123456789"));
+        Car audi = new Car("Audi", "A8 50L TDI quattro", "black", 2020, "Germany", 3f,"manual","sports car", "А000АА000",2,true,new Car.Key(true,false),new Car.Insurance(LocalDate.now().plusMonths(2),13000.0f,"987654321"));
+        Car bmw = new Car("BMW", "Z8", "black", 2021,"Germany",3f,"automatic","sedan","ВВ252ВА831",5,false, new Car.Key(false,true), new Car.Insurance(LocalDate.now().plusMonths(5), 3500.0f,"257614328"));
+        Car kia = new Car("Kia", "Sportage 4",  "red",2018, "South Korea", 2.4f,"manual","sedan","К525МН005",5,true,  new Car.Key(true,true), new Car.Insurance(LocalDate.now().plusMonths(1), 7000.0f,"628431579"));
+        Car hyundai = new Car("Hyundai", "Avante", "orange", 0, "South Korea", 1.6f,"automatic","hatch back","О325ВА125",3,false,  new Car.Key(true,true), new Car.Insurance(LocalDate.now().plusMonths(3), 8200.0f,"628931475"));
+        System.out.println(kia);
+
+
+
+
+
 
 
     }
---
+++ src/transport/Car.java
@@ -0,0 +1,200 @@
+package transport;
+
+import java.time.LocalDate;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private float engineVolume;
+    private String color;
+    private final int productionYear;
+    private final String productionCountry;
+    private String transmission;
+    private  final String bodyType;
+    private String regNumber;
+    private final int seatsCount;
+    private boolean winterTires;
+    private final Key key;
+    private final Insurance insurance;
+
+
+    public Car(String brand, String model,  String color, int productionYear, String productionCountry,float engineVolume,String transmission,String bodyType,String regNumber,int seatsCount,boolean winterTires,Key key,Insurance insurance) {
+        this.brand = validOrDefault(brand, DEFAULT_STRING_VALUE);
+        this.model = validOrDefault(model, DEFAULT_STRING_VALUE);
+        setEngineVolume(engineVolume);
+        setColor(color);
+        this.productionCountry = validOrDefault(productionCountry, DEFAULT_STRING_VALUE);
+        this.productionYear = productionYear >= 0 ? productionYear : 2000;
+        setTransmission(transmission);
+        this.bodyType = validOrDefault(bodyType, DEFAULT_STRING_VALUE);
+        setRegNumber(regNumber);
+        this.seatsCount = Math.max(seatsCount, 1);
+        setWinterTires(winterTires);
+        this.key = key;
+        this.insurance = insurance;
+    }
+
+    public static final String DEFAULT_STRING_VALUE = " default";
+
+
+    private String validOrDefault(String value, String defaultValue) {
+        if (value == null || value.isBlank()) {
+            return defaultValue;
+        }else{
+            return value;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", productionYear=" + productionYear +
+                ", productionCountry='" + productionCountry + '\'' +
+                ", transmission='" + transmission + '\'' +
+                ", bodyType='" + bodyType + '\'' +
+                ", regNumber='" + regNumber + '\'' +
+                ", seatsCount=" + seatsCount +
+                ", winterTires=" + winterTires +
+                '}';
+    }
+
+    public void setSeasonTires() {
+        int currentMonth = LocalDate.now().getMonthValue();
+        this.winterTires = currentMonth <= 4 || currentMonth >= 11;
+    }
+
+    public boolean isRegNumber() {
+        if (this.regNumber.length() != 9) {
+            return false;
+        }
+        char[] regNumberChars = this.regNumber.toCharArray();
+        return isNumberLetter(regNumberChars[0]) && isNumber(regNumberChars[1]) && isNumber(regNumberChars[2]) && isNumber(regNumberChars[3]) && isNumberLetter(regNumberChars[4]) && isNumberLetter(regNumberChars[5]) && isNumber(regNumberChars[6]) && isNumber(regNumberChars[7]) && isNumber(regNumberChars[8]);
+    }
+
+    private boolean isNumber(char symbol) {
+        return symbol >= '0' && symbol <= '9';
+    }
+
+    private boolean isNumberLetter(char symbol) {
+        String allowedSymbols = "АВЕКМНОРСТУХ";
+        return allowedSymbols.contains("" + symbol);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public int getSeatsCount() {
+        return seatsCount;
+    }
+
+    public float getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(float engineVolume) {
+        this.engineVolume = engineVolume > 0.0f ? engineVolume : 1.5f;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = validOrDefault(color, "white");
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = validOrDefault(transmission,DEFAULT_STRING_VALUE);
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = validOrDefault(regNumber,DEFAULT_STRING_VALUE);
+    }
+
+    public boolean isWinterTires() {
+        return winterTires;
+    }
+
+    public void setWinterTires(boolean winterTires) {
+        this.winterTires = winterTires;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+    public static class Key {
+        private final boolean remoteEngineStart;
+        private final boolean keyLessAccess;
+
+        public Key(boolean remoteEngineStart, boolean keyLessAccess) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keyLessAccess = keyLessAccess;
+        }
+
+        public boolean isRemoteEngineStart() {
+            return remoteEngineStart;
+        }
+
+        public boolean isKeyLessAccess() {
+            return keyLessAccess;
+        }
+    }
+
+    public static class Insurance {
+        private final LocalDate validUntil;
+        private final float cost;
+        private final String number;
+
+        public Insurance(LocalDate validUntil, float cost, String number) {
+            this.validUntil = validUntil != null ? validUntil : LocalDate.now().plusDays(10);
+            this.cost = Math.max(cost, 1f);
+            this.number = number;
+            }
+
+        public boolean isNumberValid() {
+            return number.length() == 9;
+        }
+
+        public boolean isInsuranceValid() {
+            return LocalDate.now().isBefore(this.validUntil);
+        }
+    }
+}
+
+
+
+
