6e655b68c96fc2bf2f5d699de3cafae63975bbef
kozyar <kozyarik@gmail.com>
2022-11-20 12:41:00 +0100

    задачи решены

--- src/Recipe.java
+++ src/Recipe.java
@@ -3,21 +3,21 @@ import java.util.Objects;
 import java.util.Set;
 
 public class Recipe {
-    private final String recipeName;
+//    private final String recipeName;
     private Set<Product> products;
-    private float totalCost;
+    private float totalCost = 0;
     private Integer amount;
 
-    public Recipe(String recipeName, Set<Product> products,float totalCost) {
-        this(recipeName, products, 1,totalCost);
+    public Recipe(String recipeName, Set<Product> products) {
+        this(recipeName, products, 1);
 
     }
 
-    public Recipe(String recipeName, Set<Product> products, Integer amount,float totalCost) {
-        this.recipeName = recipeName;
+    public Recipe(String recipeName, Set<Product> products, Integer amount) {
+//        this.recipeName = recipeName;
         this.products = products;
         this.amount = amount;
-        this.totalCost = calculatePrice();
+        this.totalCost = totalCost + (this.totalCost * this.amount);
     }
     public float calculatePrice() {
         float totalCoast = 0;
@@ -31,32 +31,34 @@ public class Recipe {
         return totalCost;
     }
 
-    public String getRecipeName() {
-        return recipeName;
-    }
+//    public String getRecipeName() {
+//        return recipeName;
+//    }
 
     public Set<Product> getProducts() {
         return products;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Recipe recipe = (Recipe) o;
-        return Objects.equals(recipeName, recipe.recipeName);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Recipe recipe = (Recipe) o;
+//        return Objects.equals(recipeName, recipe.recipeName);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(recipeName);
+//    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(recipeName);
-    }
 
     @Override
     public String toString() {
         return "Recipe{" +
-                "recipeName='" + recipeName + '\'' +
-                ", products=" + products +
+                "products=" + products +
+                ", totalCost=" + totalCost +
+                ", amount=" + amount +
                 '}';
     }
 }
